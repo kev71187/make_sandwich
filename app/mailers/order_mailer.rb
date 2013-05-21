69bf87c0f0b6f8d5afc19f9cf43ac7ec50a37570
Kevin <kev71187@gmail.com>
2013-05-20 20:42:13 -0700

    mailing

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,14 +1,26 @@
 class OrderMailer < ActionMailer::Base
-  default from: "kev71187@gmail.com"
+  default from: "kev71187@yahoo.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.order_maller.order_request.subject
   #
-  def order_request
-    @greeting = "Hi"
+  def order_request(user, order)
+    @order = order
+    @user = user
+    @random_quote = random_quote
+    mail to: user.email, subject: "Order Sandwich Request"
+  end
 
-    mail to: "kev71187@gmail.com", subject: "Order Sandwich Request"
+  def random_quote
+    @array = [
+      'I originally built this app with only theses instructions: "**make_sandwich is meant to test two primary categories: your proficiency in the basics of a Rails app and your personal coding strengths. The former includes creating an app, checking it in to Git multiple times as you work on it, pushing it to Heroku, and creating tests for your code. The latter involves really anything that makes you stand out or that you are really good at: fancy front-end animation, beautiful design, or clever API integration, for example. You have some time to wow us. Try to!" and I nearly built all of the extra credit mentioned in the actual project instructions',
+      '"Science can amuse and fascinate us all, but it is engineering that changes the world." - Isaac Asimov',
+      '"Real artists ship" - Steve Jobs',
+      '"Wisdom Comes From Experience, And Experience Comes From Lack Of Wisdom." - Terry Pratchett'
+    ]
+
+    @array[rand(0..(@array.length-1))]
   end
 end
