bdd1dad91c0d8896aba0a036cdca084fd993094a
Kevin <kev71187@gmail.com>
2013-05-22 00:32:21 -0700

    email

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -10,15 +10,16 @@ class OrderMailer < ActionMailer::Base
     @order = order
     @user = user
     @random_quote = random_quote
+    attachments.inline["sandwich.jpg"] = File.read(ActionController::Base.helpers.asset_path("#{Rails.root}/app/assets/images/#{@order.sandwich.title}.jpg"))
     mail to: user.email, subject: "Favor pleaseeee! :)"
   end
 
   def random_quote
     @array = [
-      'I originally built this app with only theses instructions: "**make_sandwich is meant to test two primary categories: your proficiency in the basics of a Rails app and your personal coding strengths. The former includes creating an app, checking it in to Git multiple times as you work on it, pushing it to Heroku, and creating tests for your code. The latter involves really anything that makes you stand out or that you are really good at: fancy front-end animation, beautiful design, or clever API integration, for example. You have some time to wow us. Try to!" and I nearly built all of the extra credit mentioned in the actual project instructions',
-      '"Science can amuse and fascinate us all, but it is engineering that changes the world." - Isaac Asimov',
-      '"Real artists ship" - Steve Jobs',
-      '"Wisdom Comes From Experience, And Experience Comes From Lack Of Wisdom." - Terry Pratchett',
+      #'I originally built this app with only theses instructions: "**make_sandwich is meant to test two primary categories: your proficiency in the basics of a Rails app and your personal coding strengths. The former includes creating an app, checking it in to Git multiple times as you work on it, pushing it to Heroku, and creating tests for your code. The latter involves really anything that makes you stand out or that you are really good at: fancy front-end animation, beautiful design, or clever API integration, for example. You have some time to wow us. Try to!"',
+      #'"Science can amuse and fascinate us all, but it is engineering that changes the world." - Isaac Asimov',
+      #'"Real artists ship" - Steve Jobs',
+      #'"Wisdom Comes From Experience, And Experience Comes From Lack Of Wisdom." - Terry Pratchett',
       (Typhoeus::Request.get("http://www.iheartquotes.com/api/v1/random")).body
     ]
     
